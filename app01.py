2f1e89dc889d1dbd36af11a4a57f36d4c9a4e536
jaruvit kumpun <159879441+jaruvitgiant@users.noreply.github.com>
2024-02-16 00:09:51 +0700

    Update app01.py
    add_webdemo

--- app01.py
+++ app01.py
@@ -3,13 +3,41 @@ import pandas as pd
 import numpy as np
  
 st.title('จารุวิทย์ คำพันธ์ 66114540131')
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+import streamlit as st
+import pandas as pd
 
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+
+st.write("This is introduction to streamlit")
+
+st.markdown("## Code")
+code = '''
+def hello():
+    print("Hello!")
+'''
+
+show_btn = st.button("Show code!")
+if show_btn:
+    st.code(code, language='python')
+
+
+cols = st.columns(2)
+with cols[0]:
+    age_inp = st.number_input("Input your age")
+    st.markdown(f"Your age is {round(age_inp, 2)}")
+
+
+# st.markdown("# NLP Task")
+with cols[1]:
+    text_inp = st.text_input("Input your text")
+    word_tokenize = "|".join(text_inp.split())
+    st.markdown(f"{word_tokenize}")
+
+
+df = pd.DataFrame({
+'first column': [1, 2, 3, 4],
+'second column': [10, 20, 60, 90]
+})
+st.dataframe(df)
+show_chart_btn = st.button("Show Chart!!")
+if show_chart_btn:
+    st.line_chart(df, x='first column', y='second column')
